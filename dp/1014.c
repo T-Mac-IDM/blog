8073493d661e8bcd1cec129858ea792e64051514
T-Mac-IDM <t_mac_idm@163.com>
2015-11-05 12:59:22 +0800

    1014 ac

--- dp/1014.c
+++ dp/1014.c
@@ -1,38 +1,109 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file 1014.c
+ * @author liuzhaocheng(com@baidu.com)
+ * @date 2015/11/05 10:12:30
+ * @brief 
+ *  
+ **/
+
 #include <stdio.h>
 
-//nums of original marbles at every value
-int nums[120001];
-//after binary-tunning, every single marble at every value
-int things[20001];
-//dp size = 20000*6/2 = 60000
-int dp[60001];
+#define MAXIMUM_NUM 20000
 
-void binary_tune() {
-	int index_things = 0;
-	for (int i = 0;i < 6;i++) {
-		int ori = nums[i];
-		int tmp = 1;
-		while (ori != 0) {
-			if ((ori % 2) == 1) {
-				things[index_things] = tmp;
-				nums[tmp]
-				index_things++;
-			}			
-			tmp = tmp << 1;
-			ori = ori >> 1;	
-		}
-	}
-	for(int i = 0;i < 8;i++)
-		printf("%d ",things[i]);
+int N[7];
+int pack[120001];
+
+int sum_values() {
+    int sum = 0;
+    int i = 1;
+    for(; i <= 6; i++)
+        sum += N[i]*i;
+    return sum;
+}
+
+int pack01(int lens, int new_value) {
+    //printf("%d\n", new_value);
+    int i = lens;
+    for (; i >= 0; i--) {
+        if ((i >= new_value) && (pack[i - new_value] != -1) && (pack[i - new_value] + new_value > pack[i])) {
+            pack[i] = pack[i - new_value] + new_value;
+            //printf("%d %d %d %d\n", i, new_value, pack[i - new_value], pack[i]);
+        }
+    }
+    if (pack[lens] != -1)
+        return 1;
+    else
+        return 0;
 }
 
 int main() {
-	while(scanf("%d %d %d %d %d %d", nums+1, nums+2, nums+3, nums+4, nums+5, nums+6)) {
-		if ((nums[1] | nums[2] | nums[3] | nums[4] | nums[5] | nums[6]) == 0)
-			break;
-		binary_tune();
-		//for (int i = 0; i < 6; i++)
-		//	printf("%d ", nums[i]); 
-		//printf("\n");
-	}
+    int index = 0;
+    while(1) {
+        scanf("%d %d %d %d %d %d", N+1, N+2, N+3, N+4, N+5, N+6);
+        if (!(N[1] | N[2] | N[3] | N[4] | N[5] | N[6]))
+            break;
+        index++;
+        int sum = sum_values();
+        if (sum%2) {
+            printf("Collection #%d:\nCan't be divided.\n", index);
+            printf("\n");
+            continue;
+        }
+        int lens = sum/2;
+        int i = 1;
+        int flag = 0;
+        pack[0] = 0;
+        for (; i <= lens; i++) {
+            pack[i] = -1;
+        }
+        i = 1;
+        for (; i <= 6; i++) {
+            int cur_num = N[i];
+            int binary_num = 1;
+            while(binary_num < cur_num) {
+                int new_value = binary_num * i;
+                cur_num -= binary_num;
+                flag = pack01(lens, new_value);
+                binary_num *= 2;
+            } 
+            if (flag)
+                break;
+            flag = pack01(lens, cur_num * i);
+        } 
+        if (flag)
+            printf("Collection #%d:\nCan be divided.\n", index);
+        else
+            printf("Collection #%d:\nCan't be divided.\n", index);
+        printf("\n");
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
